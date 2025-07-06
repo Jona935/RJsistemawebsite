f687980679e1b043303c794f2d9b4322b3adeb4d
Jonathan Rios <jonarios2511@gmail.com>
2025-07-06 15:58:21 +0000

    parpadea muy rapido el

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -89,8 +89,8 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'border-pulse': 'border-pulse 2.5s cubic-bezier(0.4, 0, 0.6, 1) infinite',
-        'icon-pulse': 'icon-pulse 2.5s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        'border-pulse': 'border-pulse 3.5s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        'icon-pulse': 'icon-pulse 3.5s cubic-bezier(0.4, 0, 0.6, 1) infinite',
       },
     },
   },
